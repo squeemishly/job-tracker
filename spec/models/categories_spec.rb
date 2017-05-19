1916850616e51587ed491eae1061199f763dad25
squeemishly <squeemishly@gmail.com>
2017-05-18 20:23:14 -0600

    added testing for Category model

---
+++ spec/models/categories_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Category do
+  it "has a title" do
+    category = Category.create(title: "Inspector")
+
+    expect(category).to be_valid
+  end
+
+  it "must have a title" do
+    category = Category.create
+
+    expect(category).to be_invalid
+  end
+
+  it "must have a unique title" do
+    category = Category.create(title: "Inspector")
+    category2 = Category.create(title: "Inspector")
+
+    expect(category).to be_valid
+    expect(category2).to be_invalid
+  end
+end
