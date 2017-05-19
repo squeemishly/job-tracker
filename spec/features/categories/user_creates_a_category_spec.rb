afc43b0cbe48b161eedb0a3858829fe8f7853cb1
squeemishly <squeemishly@gmail.com>
2017-05-18 20:18:26 -0600

    user can add a category and be taken to a page that shows the category title

--- spec/features/categories/user_creates_a_category_spec.rb
+++ spec/features/categories/user_creates_a_category_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe "A user can create a new category" do
 
     expect(page).to have_content "Development"
   end
+
+  it "sends them back to the form if the user tries to create a category that already exists" do
+    pending
+    Category.create(title: "Training")
+
+    visit categories_path
+    click_on "Create a New Category"
+    fill_in "category[title]", with: "Training"
+    click_on "Create Category"
+
+    expect(current_path).to eq(new_category_path)
+
+  end
 end
