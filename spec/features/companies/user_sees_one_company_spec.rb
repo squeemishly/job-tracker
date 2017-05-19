b7421773dc28bed993db64a059bffb2a4fe0adfe
squeemishly <squeemishly@gmail.com>
2017-05-19 15:23:58 -0600

    added navigation in browser

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -7,8 +7,11 @@ describe "User sees one company" do
 
     visit company_path(company)
 
+    expect(page).to have_content("All Jobs for #{company.name}")
     expect(current_path).to eq("/companies/#{company.id}/jobs")
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
+
+    expect(page).to have_link("Add a New Job", href: "/companies/#{company.id}/jobs/new")
   end
 end
--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe "User creates a new job" do
   scenario "a user can create a new job" do
     company = Company.create!(name: "ESPN")
+    category = Category.create(title: "Nursing")
     visit new_company_job_path(company)
 
     fill_in "job[title]", with: "Developer"
     fill_in "job[description]", with: "So fun!"
     fill_in "job[level_of_interest]", with: 80
     fill_in "job[city]", with: "Denver"
+    select "Nursing", from: "job[category]"
 
     click_button "Create"
 
@@ -17,5 +19,6 @@ describe "User creates a new job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("80")
     expect(page).to have_content("Denver")
+    expect(page).to have_content("Nursing")
   end
 end
