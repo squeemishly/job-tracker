10b976a4cde8bea1479d04f450b6173fc35f3ac2
squeemishly <squeemishly@gmail.com>
2017-05-21 13:50:58 -0600

    all jobs in a specific category now show up on the category show page

--- spec/features/categories/user_sees_one_category_spec.rb
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "A user can see a single category" do
   it "when they click ont he category name from the index" do
+    company = create(:company)
     category = create(:category)
-
+    job = create(:job, company_id: company.id, category_id: category.id)
+    
     visit categories_path
     click_on category.title
 
     expect(page).to have_css('h3', text: category.title)
+    expect(page).to have_css('h5', text: "Jobs in #{category.title} category")
+    expect(page).to have_link(job.title, href: company_job_path(company, job))
     expect(page).to have_link('Return to Categories List', href: categories_path)
   end
 end
