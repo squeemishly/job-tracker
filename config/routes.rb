2cc8111622fc558db12e1922387d31d6ce368328
squeemishly <squeemishly@gmail.com>
2017-05-21 12:44:08 -0600

    set comments routes to except :delete

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :jobs
   end
 
-  resources :categories
+  resources :categories, except: [:delete]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
